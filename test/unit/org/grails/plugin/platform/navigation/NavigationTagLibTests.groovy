8d664e6e4373fbd34ded165f4fc85895b7bb5c13
Marc Palmer <marc@anyware.co.uk>
2013-04-18 18:15:34 +0100

    New test for visible context items

---
+++ test/unit/org/grails/plugin/platform/navigation/NavigationTagLibTests.groovy
@@ -0,0 +1,38 @@
+package org.grails.plugin.platform.navigation
+
+import grails.test.mixin.TestFor
+
+import org.grails.plugin.platform.NavigationTagLib
+
+@TestFor(NavigationTagLib)
+class NavigationTagLibTests {
+
+	void testIsVisibleClosureHasCorrectContext() {
+        boolean isVisibleCalled = false
+		def testNode = new NavigationItem([
+			linkArgs:[action:'home'],
+			visible: { ->
+				assert item
+				isVisibleCalled = true
+		}])
+        def parentNode = new NavigationItem([
+            linkArgs:[controller:'test'],
+            children:[testNode]
+        ])
+
+        def mockNav = [:]
+        mockNav.nodesForPath = { String path ->
+ 			[parentNode]
+ 		}
+        mockNav.nodeForId = { String id ->
+            parentNode
+        }
+        mockNav.getDefaultScope = { request -> 'app' }
+
+        tagLib.grailsNavigation = mockNav
+
+		def output = applyTemplate("<nav:menu path='app'/>", [:]);
+
+		assert isVisibleCalled
+	}
+}
